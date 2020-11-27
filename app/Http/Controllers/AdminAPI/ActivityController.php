f34b97fc59bcf32c682b9f97e895ac3446e3cb87
melonz28 <kusman758@gmail.com>
2020-11-27 22:29:20 +0800

    Heroku :launch:

---
+++ app/Http/Controllers/AdminAPI/ActivityController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\AdminAPI;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $activities = auth()->user()->actions->load('causer');
+        
+        $activities = auth()->user()->actions()->with('causer')->latest()->get();
+    	// $activities = Activity::with('causer')->where('causer_id', auth()->user()->id)->latest()->get();
+        return $activities;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
